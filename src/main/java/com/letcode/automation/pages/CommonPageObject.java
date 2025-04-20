e35d781426dac18585fb328d2a80f01135cffc29
PalashTirpude <palashtirpude100@gmail.com>
2025-04-20 14:36:00 +0530

    chore : update navigateToCardLink method

--- src/main/java/com/letcode/automation/pages/CommonPageObject.java
+++ src/main/java/com/letcode/automation/pages/CommonPageObject.java
@@ -3,19 +3,20 @@ package com.letcode.automation.pages;
 import com.central.framework.genericutils.DriverWait;
 import com.central.framework.genericutils.JavaScriptOperations;
 import com.central.framework.genericutils.WebDriverActions;
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-
+@Slf4j
 public class CommonPageObject {
 
 
 
     public static void navigateToCardLink(String cardName){
-        By cardLocator=By.xpath(String.format("//div[./header/p[text()=' %s ']]/child::footer/a",cardName));
+        String xpath=String.format("//div[./header/p[text()=' %s ']]/child::footer/a",cardName);
+        By cardLocator=By.xpath(xpath);
         DriverWait.waitElementToBeClickable(cardLocator,5);
-        int index=cardName.equals("Table_Advance")?2:1;
-        WebElement element= WebDriverActions.findElements(cardLocator).get(index);
-        JavaScriptOperations.scrollToElement(cardLocator);
-        element.click();
+//        int index=cardName.equals("Table_Advance")?1:0;
+//        WebElement element= WebDriverActions.findElements(cardLocator).get(index);
+        JavaScriptOperations.clickElement(cardLocator);
     }
 }
