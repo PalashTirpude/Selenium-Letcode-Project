90818dbee961c54fe7b00b44a2d367b9078add04
PalashTirpude <palashtirpude100@gmail.com>
2025-07-07 16:32:04 +0530

    fix : compilation warnings

--- src/test/java/com/letcode/automation/test/SimpleTableTest.java
+++ src/test/java/com/letcode/automation/test/SimpleTableTest.java
@@ -21,9 +21,10 @@ public class SimpleTableTest {
         context=new AnnotationConfigApplicationContext(SimpleTableConfig.class);
     }
 
+    @SuppressWarnings("rawtypes")
     @Test(priority =1, description = "Check summation of prices of items = Total in table footer")
     private void checkTotalPrice(){
-        Map<String,String> list=context.getBean("fetchShoppingListTable", Map.class);
+        Map list=context.getBean("fetchShoppingListTable", Map.class);
         CustomAssert.assertTrueHard(context.getBean("fetchShoppingListTable", Map.class).containsKey("Corn"),"Check table List contains key :  Corn");
         CustomAssert.assertEqualsHard(context.getBean("fetchShoppingListTableCalculatedTotalPrice",Integer.class)
                 ,context.getBean("fetchShoppingListTableCalculatedTotalPrice",Integer.class),"Check TotalPrice");
